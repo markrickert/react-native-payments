ad6cf80ba0f3dde55e969a3ca7e7d752a3f311a5
Mark Rickert <mjar81@gmail.com>
2017-10-04 12:10:40 -0600

    Removes all payment networks except the big 4.
    Fixes runtime crash.

--- packages/react-native-payments/lib/ios/ReactNativePayments.m
+++ packages/react-native-payments/lib/ios/ReactNativePayments.m
@@ -209,16 +209,9 @@ RCT_EXPORT_METHOD(handleDetailsUpdate: (NSDictionary *)details
     // https://developer.apple.com/reference/passkit/pkpaymentnetwork
     NSDictionary *supportedNetworksMapping = @{
                                                @"amex": PKPaymentNetworkAmex,
-                                               @"chinaunionpay": PKPaymentNetworkChinaUnionPay,
                                                @"discover": PKPaymentNetworkDiscover,
-                                               @"jcb": PKPaymentNetworkJCB,
                                                @"mastercard": PKPaymentNetworkMasterCard,
-                                               @"privatelabel": PKPaymentNetworkPrivateLabel,
-                                               @"visa": PKPaymentNetworkVisa,
-                                               @"interac": PKPaymentNetworkInterac,
-                                               @"suica": PKPaymentNetworkSuica,
-                                               @"quicpay": PKPaymentNetworkQuicPay,
-                                               @"idcredit": PKPaymentNetworkIDCredit
+                                               @"visa": PKPaymentNetworkVisa
                                                };
 
     // Setup supportedNetworks
